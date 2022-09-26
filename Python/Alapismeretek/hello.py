e4c5ac79bd7a11042c00632f6bbbbaf399f8afc7
farkas.norbert <farkas.norbert@ckik.hu>
2022-09-26 09:44:29 +0200

    print

---
+++ Python/Alapismeretek/hello.py
@@ -0,0 +1,14 @@
+print("Hello\nWorld!")
+print("Hello World!")
+# Egy soros komment
+print("Hello World!")
+# prit('Hello World!')
+"""
+Bármi ami itt van,
+az több soros komment!
+"""
+'''
+Bármi ami itt van,
+az több soros komment!
+'''
+print("Hello World!")
---
+++ Python/Alapismeretek/print.py
@@ -0,0 +1,4 @@
+print("alma", "fa")
+print("alma", "fa", "barack", "fa", sep="\n")
+print("alma" + "fa")
+print("almafa")
