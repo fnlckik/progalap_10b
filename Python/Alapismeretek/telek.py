00589ce22f0cc83bc23d7c06ca792e4ce3d63e98
farkas.norbert <farkas.norbert@ckik.hu>
2022-09-28 10:35:09 +0200

    telek

---
+++ Python/Alapismeretek/telek.py
@@ -0,0 +1,12 @@
+#Konstansok
+hossz = 45
+szelesseg = 35
+kapu = 5
+
+#Szamolas
+kerites = 2*(hossz + szelesseg) - kapu
+alapterulet = hossz * szelesseg
+
+#Kiiras
+print("A kerites hossza:", kerites, "meter.")
+print("A telek alapterulete:", alapterulet, "nm.")
