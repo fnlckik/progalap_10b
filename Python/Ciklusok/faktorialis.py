14d0aee38d6bdab068a1b3aa66380e8d3bf789b5
farkas.norbert <farkas.norbert@ckik.hu>
2022-10-12 10:34:57 +0200

    hello_szamok_negyzetszam

---
+++ Python/Ciklusok/faktorialis.py
@@ -0,0 +1,5 @@
+n = int(input("n: "))
+
+eredmeny = 1
+for i in range(n):
+    
---
+++ Python/Ciklusok/szamok.py
@@ -0,0 +1,13 @@
+# 100 95 90 85 80 ... 0
+'''
+for i in range(100, -1, -5):
+    print(i)
+'''
+
+'''
+for i in range(0, 101, 5):
+    print(100-i)
+'''
+
+for i in range(-100, 1, 5):
+    print(i*(-1))
