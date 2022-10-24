8536034987e8e6b7215a8bf1e2dfa1b284bdf1a6
farkas.norbert <farkas.norbert@ckik.hu>
2022-10-24 09:40:55 +0200

    primszam

---
+++ Python/Ciklusok/primszam.py
@@ -0,0 +1,19 @@
+#Be: n (egesz)
+#Ki: primszam-e? (Primszam / Nem primszam)
+
+n = int(input("n: "))
+
+primE = True
+
+i = 2
+while i < n and primE:
+    if n % i == 0:
+        primE = False
+        #print("Nem primszam!")
+    i += 1
+
+#print(primE)
+if primE:
+    print("Prímszám!")
+else:
+    print("Nem prímszám!")
