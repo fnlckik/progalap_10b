38661cafb8f48a225120da4f72b8ef9c773b65f2
farkas.norbert <farkas.norbert@ckik.hu>
2022-11-11 09:44:39 +0100

    randint

---
+++ Python/Modulok, függvények/erme.py
@@ -0,0 +1,7 @@
+from random import *
+
+erme = randint(1, 2) #1 = fej, 2 = iras
+if erme == 1:
+    print("F")
+else:
+    print("I")
--- Python/Modulok, függvények/veletlen.py
+++ Python/Modulok, függvények/veletlen.py
@@ -1,6 +1,23 @@
 from random import *
 
-for i in range(10):
+egy = 0
+ketto = 0
+harom = 0
+
+for i in range(500000):
     #veletlen eloallitasa
-    szam = round(random() * 2) + 1 
-    print(szam)
+    #szam = round(random() * 2) + 1 #ROSSZ
+    #szam = int(random() * 3) + 1 #HELYES
+    szam = randint(1, 3)
+    print(szam, end=" ")
+    if szam == 1:
+        egy += 1
+    elif szam == 2:
+        ketto += 1
+    else:
+        harom += 1
+
+print()
+print("Egyesek:", egy)
+print("Kettesek:", ketto)
+print("Harmasok:", harom)
