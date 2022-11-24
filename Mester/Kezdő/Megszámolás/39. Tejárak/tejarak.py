7631249e8ebce4400686d9639b8b8e650d66976b
farkas.norbert <farkas.norbert@ckik.hu>
2022-11-24 11:10:22 +0100

    tejarak_vege

--- Mester/Kezdő/Megszámolás/39. Tejárak/tejarak.py
+++ Mester/Kezdő/Megszámolás/39. Tejárak/tejarak.py
@@ -1,3 +1,4 @@
+#Beolvasas
 #n, p, q beolvasas
 be = input()
 lista = be.split()
@@ -11,4 +12,13 @@ tejarak = []
 for i in range(n):
     ar = int(input())
     tejarak.append(ar)
-print(tejarak)
+#print(tejarak)
+
+#Feldolgozas (megszamolas tetel)
+db = 0
+for i in range(n):
+    if p <= tejarak[i] and tejarak[i] <= q:
+        db += 1
+
+#Kiiras
+print(db)
