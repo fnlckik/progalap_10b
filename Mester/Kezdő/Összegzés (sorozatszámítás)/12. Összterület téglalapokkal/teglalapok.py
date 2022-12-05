6c7f53b0d7f8515c85058986f7f264053ffcfe82
farkas.norbert <farkas.norbert@ckik.hu>
2022-12-05 09:40:52 +0100

    teglalapok_beolvasas

---
+++ Mester/Kezdő/Összegzés (sorozatszámítás)/12. Összterület téglalapokkal/teglalapok.py
@@ -0,0 +1,10 @@
+n = int(input())
+hosszusagok = []
+szelessegek = []
+for i in range(n):
+    sor = input().split()
+    sz = int(sor[0])
+    h = int(sor[1])
+    hosszusagok.append(h)
+    szelessegek.append(sz)
+
