f65025b68f6619e4dc2363dc478371225b66589e
farkas.norbert <farkas.norbert@ckik.hu>
2022-12-07 10:25:26 +0100

    teglalap_vege

--- Mester/Kezdő/Összegzés (sorozatszámítás)/12. Összterület téglalapokkal/teglalapok.py
+++ Mester/Kezdő/Összegzés (sorozatszámítás)/12. Összterület téglalapokkal/teglalapok.py
@@ -7,4 +7,21 @@ for i in range(n):
     h = int(sor[1])
     hosszusagok.append(h)
     szelessegek.append(sz)
+# print(hosszusagok, szelessegek)
 
+osszeg = 0
+for i in range(n):
+    aktTer = szelessegek[i] * hosszusagok[i]
+    osszeg += aktTer
+    #osszeg = osszeg + aktTer
+
+'''
+osszeg = 0
+i = 0
+while i < n:
+    aktTer = szelessegek[i] * hosszusagok[i]
+    osszeg += aktTer
+    i += 1
+'''
+
+print(osszeg)
