9e56450634a24a9f78d90aa2c8d61c31eed4980f
farkas.norbert <farkas.norbert@ckik.hu>
2022-12-08 10:58:40 +0100

    legidosebb_dolgozo_vege

--- Mester/Kezdő/MinMax/8. Legidősebb dolgozó/legidosebb_dolgozo.py
+++ Mester/Kezdő/MinMax/8. Legidősebb dolgozó/legidosebb_dolgozo.py
@@ -1,5 +1,18 @@
+#Beolvasas
 n = int(input())
 korok = []
 fizetesek = []
 for i in range(n):
-    
+    sor = list(map(int, input().split()))
+    korok.append(sor[0])
+    fizetesek.append(sor[1])
+#print(korok, fizetesek)
+
+#Maximum
+maxi = 0
+for i in range(1, n):
+    if korok[i] > korok[maxi]:
+        maxi = i
+
+#Kiiras
+print(fizetesek[maxi])
